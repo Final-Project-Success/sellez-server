35b56c7dd972a65b9d3a10f4adca116559e6f600
SAFRIZAL AJI <safrizalaji16@gmail.com>
2023-01-04 21:32:48 +0700

    feat: crud orderproducts

---
+++ services/order-products/migrations/20230104142106-create-order-product.js
@@ -0,0 +1,43 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("OrderProducts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      ProductId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      OrderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      subTotal: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("OrderProducts");
+  },
+};
