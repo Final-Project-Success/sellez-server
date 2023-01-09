fd535062b98ac8cadb479b65a3a042024f9e499d
SAFRIZAL AJI <safrizalaji16@gmail.com>
2023-01-09 16:56:49 +0700

    pull dev

--- services/server/routes/users.js
+++ services/server/routes/users.js
@@ -1,11 +1,11 @@
 const Controller = require("../controllers/users");
-// const authentication = require("../middlewares/authentication");
+const { authentication } = require("../middlewares/authentication");
 const router = require("express").Router();
 
 router
   .post("/login", Controller.login)
   .post("/register", Controller.register)
   .post("/login-oauth", Controller.oauthLogin)
-  .get("/verification", Controller.verificationEmail);
+  .get("/verification", authentication, Controller.verificationEmail);
 
 module.exports = router;
