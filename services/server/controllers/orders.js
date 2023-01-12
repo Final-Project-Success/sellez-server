9f983a31f92d309e571a9ca5d908c3d21edcf19d
aditnuryana <aditnuryana02@gmail.com>
2023-01-12 15:33:12 +0700

    pull dev

--- services/server/controllers/orders.js
+++ services/server/controllers/orders.js
@@ -33,8 +33,8 @@ class Controller {
           description: `Invoice for ${idPayout}`,
           amount: totalPrice,
           items: mapping,
-          successRedirectURL: "https://www.google.com",
-          failureRedirectURL: "https://www.google.com",
+          successRedirectURL: "https://sellez-web.web.app/orders",
+          failureRedirectURL: "https://sellez-web.web.app/orders",
           fees: [
             {
               type: "Handling Fee",
